be170b4a5911f931c34f9280d86af0eab9eab049
sahar <sahar.bit@gmail.com>
2019-02-11 22:23:05 +0200

    Fixed small bug over missing code

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@ export default {
 .app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale
+  -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
 </style>
