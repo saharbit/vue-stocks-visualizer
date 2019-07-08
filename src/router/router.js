835a0e84e5757ab089e0c6660ea56a30f0b862c8
Sahar <sahar.bit@gmail.com>
2019-07-08 20:30:49 +0300

    Rename

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Main from '../views/Main.vue';
+import Home from '../views/Home.vue';
 
 Vue.use(Router);
 
@@ -11,7 +11,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Main
+      component: Home
     }
   ]
 });
